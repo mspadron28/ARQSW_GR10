f9d02bd2be94d17c9b43e9b8764e8903ea2e8647
MateoRoman <cmroman1@espe.edu.ec>
2025-05-07 19:13:11 -0500

    .Net Cliente Web

---
+++ UNIDAD 1/00_SOA_SIN_BDD/001. SOAP_DOTNET_CONUNI_GR10/02. CLIENTE_WEB/CLIWEB/CLIWEB/Modelo/ConversionModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CLIWEB.Modelo
+{
+    public class ConversionModel
+    {
+        public double InputValue { get; set; }
+        public string ConversionType { get; set; }
+        public double? Result { get; set; }
+
+        public List<string> ConversionTypes => new List<string>
+        {
+            "CentimetersToFeet",
+            "FeetToCentimeters",
+            "MetersToYards",
+            "YardsToMeters",
+            "InchesToCentimeters",
+            "CentimetersToInches"
+        };
+    }
+}
--- UNIDAD 1/00_SOA_SIN_BDD/001. SOAP_DOTNET_CONUNI_GR10/02. CLIENTE_WEB/Cliente_Web_SOAP/Cliente_Web_SOAP/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cliente_Web_SOAP.Global" Language="C#" %>
