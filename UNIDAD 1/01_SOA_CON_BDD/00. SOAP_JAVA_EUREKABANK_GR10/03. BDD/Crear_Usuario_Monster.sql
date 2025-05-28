253f200f68579ec255680d13dcc07a81a85c4c2c
mspadron28 <matiaspadron2@gmail.com>
2025-05-27 22:37:43 -0500

    Depuración carpetas y agregar estructura base de dotnet con bdd

--- UNIDAD 1/01_SOA_CON_BDD/00. SOAP_JAVA_EUREKABANK_GR10/03. BDD/Crear_Usuario_Monster.sql
+++ UNIDAD 1/01_SOA_CON_BDD/00. SOAP_JAVA_EUREKABANK_GR10/03. BDD/Crear_Usuario_Monster.sql
@@ -1,29 +1,20 @@
--- Insertar nuevo empleado
+-- Insertar nuevo empleado con usuario y clave
 INSERT INTO Empleado (
   chr_emplcodigo,
   vch_emplpaterno,
   vch_emplmaterno,
   vch_emplnombre,
   vch_emplciudad,
-  vch_empldireccion
+  vch_empldireccion,
+  vch_emplusuario,
+  vch_emplclave
 ) VALUES (
   '0015',
   'Monster',
   'Inc',
   'James Sullivan',
   'Monstropolis',
-  'Avenida del Terror 1313'
-);
-
--- Insertar nuevo usuario (clave en texto plano)
-INSERT INTO Usuario (
-  chr_emplcodigo,
-  vch_emplusuario,
-  vch_emplclave,
-  vch_emplestado
-) VALUES (
-  '0015',
+  'Avenida del Terror 1313',
   'MONSTER',
-  'MONSTER9',
-  'ACTIVO'
+  'MONSTER9'
 );
